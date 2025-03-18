788b8785dfabb72c09ceca527cbcda7096fab5b2
mansovanna <mansovanna2022@gmail.com>
2025-03-18 11:33:59 +0700

    v.0.1

---
+++ js/app.js
@@ -0,0 +1,22 @@
+console.log("HELLO");
+
+const buttonShow = document.getElementById("password-btn");
+const password = document.getElementById("password");
+const svg1 = document.getElementById("svg-pass1");
+const svg2 = document.getElementById("svg-pass2");
+
+if (buttonShow && password && svg1 && svg2) {
+    buttonShow.addEventListener("click", () => {
+        if (password.type === "password") {
+            password.type = "text"; 
+            svg1.style.display = "none"; 
+            svg2.style.display = "flex"; 
+            svg2.style.stroke = "red";
+        } else {
+            password.type = "password"; 
+            svg1.style.display = "flex"; 
+            svg2.style.display = "none"; 
+            svg2.style.stroke = "red";
+        }
+    });
+}
